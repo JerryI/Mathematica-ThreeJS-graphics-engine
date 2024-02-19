240875f665bb6a0a1c7b538a3e31b8e984c4624a
Kirill <krikus.ms@gmail.com>
2024-02-19 15:34:08 +0100

    update

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,15 +4,26 @@ import json from "@rollup/plugin-json";
 
 import serve from "rollup-plugin-serve";
 
+import terser from '@rollup/plugin-terser';
+
 export default {
 
   input: 'src/kernel.js',
   
-  output: {
-    dir: 'dist/',
-    format: "es",
-    strict: false
-  },
+  output: [
+    {
+      file: 'dist/kernel.min.js',
+      format: "es",
+      strict: false,
+      manualChunks: () => 'merged',
+      plugins: [terser()]
+    },
+    {
+      dir: 'dist/',
+      format: "es",
+      strict: false
+    }
+  ],
   plugins    : [
   nodeResolve({
     jsnext: true,
